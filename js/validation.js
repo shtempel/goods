1676090b4842b274054a817ec8e09954c6683789
shtempel <flag00@gmail.com>
2017-11-08 01:31:35 +0400

    refactor

---
+++ js/validation.js
@@ -0,0 +1,70 @@
+'use strict';
+
+TABLEAPP.validation = (function () {
+    var nameFieldId, emailFieldId, validExp;
+    nameFieldId = $('#name-add-field');
+    emailFieldId = $('#email-add-field');
+    var validation = {
+
+        showErrors: function (type) {
+            switch (type) {
+                case 'name':
+                    nameFieldId.addClass('border-error');
+                    nameFieldId.next().addClass('visible');
+                    break;
+                case 'email':
+                    emailFieldId.addClass('border-error');
+                    emailFieldId.next().addClass('visible');
+                    break;
+            }
+        },
+
+        nameFieldValidation: function (nameFieldId) {
+            if ($(nameFieldId).val() === '') {
+                return validation.showErrors('name');
+            } else {
+                return true;
+            }
+        },
+
+        emailFieldValidation: function (emailFieldId) {
+            var r = (/(?:^[A-Za-z]+@[A-Za-z]+\.[A-Za-z]{2,}$)/g);
+            if (!r.test($(emailFieldId).val())) {
+                return validation.showErrors('email');
+            } else {
+                return true;
+            }
+        }
+    };
+
+    (function () {
+        $('#name-add-field').keypress(function () {
+            if (validation.nameFieldValidation('#name-add-field')) {
+                nameFieldId.removeClass('border-error');
+                nameFieldId.next().removeClass('visible');
+            }
+        }).blur(function () {
+            validation.nameFieldValidation('#name-add-field');
+        }).keyup(function () {
+            var str = nameFieldId.val();
+            if (str.length > 15) {
+                nameFieldId.val(str.substr(0, str.length - 1));
+            }
+        });
+
+        emailFieldId.keyup(function () {
+            if (validation.emailFieldValidation('#email-add-field')) {
+                emailFieldId.removeClass('border-error');
+                emailFieldId.next().removeClass('visible');
+            }
+        }).blur(function () {
+            validation.emailFieldValidation('#email-add-field');
+        });
+
+        $('#')
+
+    })();
+
+    return validation;
+
+})();
